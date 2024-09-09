1d3d0f7cc09e537c87927f127997b8660e8e08dc
chuan <3153917921@qq.com>
2024-09-09 18:38:52 +0800

    更新

---
+++ generated/models/IPageProblemTitleVO.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ProblemTitleVO } from './ProblemTitleVO';
+export type IPageProblemTitleVO = {
+    size?: number;
+    records?: Array<ProblemTitleVO>;
+    current?: number;
+    total?: number;
+    pages?: number;
+};
+
