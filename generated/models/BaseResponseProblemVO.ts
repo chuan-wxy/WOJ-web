6bf120d4976c5dee66c42a3c0b5f63fcb47617bd
chuan <3153917921@qq.com>
2024-09-10 11:44:29 +0800

    更新

---
+++ generated/models/BaseResponseProblemVO.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { ProblemVO } from './ProblemVO';
+export type BaseResponseProblemVO = {
+    code?: number;
+    message?: string;
+    data?: ProblemVO;
+};
+
