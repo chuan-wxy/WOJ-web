db33d82920cdcbbdfdfe7f3f877331ac557c6c7d
chuan <3153917921@qq.com>
2024-09-11 13:46:30 +0800

    更多的角色权限

--- src/access/accessEnum.ts
+++ src/access/accessEnum.ts
@@ -3,7 +3,14 @@
  */
 const ACCESS_ENUM = {
   NOT_LOGIN: "notLogin",
-  USER: "user",
+  ROOT: "root",
   ADMIN: "admin",
+  DEFAULT_USER: "default_user",
+  NO_SUBMIT_USER: "no_subimit_user",
+  NO_DISCUSS_USER: "no_discuss_user",
+  MUTE_USER: "mute_user",
+  NO_SUBMIT_NO_DISCUSS_USER: "no_submit_no_discuss_user",
+  NO_SUBMIT_MUTE_USER: "no_submit_mute_user",
+  PROBLEM_ADMIN: "problem_admin",
 };
 export default ACCESS_ENUM;
