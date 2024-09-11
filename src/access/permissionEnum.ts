d0f06956da6ed2db824103a379348b68eb3e933b
chuan <3153917921@qq.com>
2024-09-11 16:38:36 +0800

    权限枚举类

---
+++ src/access/permissionEnum.ts
@@ -0,0 +1,12 @@
+/**
+ * 权限定义
+ */
+
+const PERMISSION_ENUM = {
+  SUBMIT: "submit",
+  POST_MESSAGE: "post_message",
+  DISCUSS: "discuss",
+  MANGE: "mange",
+  REPLY: "reply",
+};
+export default PERMISSION_ENUM;
