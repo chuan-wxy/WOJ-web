fae79e8d027394d9f871313a1fd15bea51fba9b1
chuan <3153917921@qq.com>
2024-09-11 17:53:09 +0800

    新增权限

--- src/access/permissionEnum.ts
+++ src/access/permissionEnum.ts
@@ -7,6 +7,8 @@ const PERMISSION_ENUM = {
   POST_MESSAGE: "post_message",
   DISCUSS: "discuss",
   MANGE: "mange",
+  PROBLEM_MANGE: "problem_mange",
   REPLY: "reply",
+  NO: "no",
 };
 export default PERMISSION_ENUM;
