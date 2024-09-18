ddfe866519e4d563806310d19abb97d0ddf77fe5
chuan <3153917921@qq.com>
2024-09-18 22:27:18 +0800

    将数据对外暴露

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -4,45 +4,33 @@
 
 <script setup lang="ts">
 import * as monaco from "monaco-editor";
-import { defineProps, onMounted, ref, toRaw, withDefaults } from "vue";
-import { useCommonStore } from "@/store/CommonStore";
+import { defineExpose, onMounted, ref } from "vue";
 
 const codeEditorRef = ref();
-const commonStore = useCommonStore();
+const codeEditorData = ref();
 
-const n = ref(commonStore.webStyle);
+defineExpose({
+  codeEditorData,
+});
 
-const props = withDefaults(
-  defineProps<{
-    codeValue: string;
-    handleChange: (v: string) => void;
-  }>(),
-  {
-    codeValue: "",
-    handleChange: (v: string) => {
-      console.log(v);
-    },
-  }
-);
 onMounted(() => {
   if (!codeEditorRef.value) {
     return;
   }
   const editor = monaco.editor.create(codeEditorRef.value, {
-    value: props.codeValue,
+    value: codeEditorData.value,
     language: "c", // 语言
-    automaticLayout: true, // 宽度自适应
+    // automaticLayout: true, // 宽度自适应
     lineNumbers: "on", // 行号
     roundedSelection: false,
-    scrollBeyondLastLine: false,
+    scrollBeyondLastLine: true,
     readOnly: false,
     theme: "vs-dark",
   });
-  if (editor) {
-    editor.onDidChangeModelContent((e) => {
-      props.handleChange(editor.getValue());
-    });
-  }
+  const model = editor.getModel();
+  model?.onDidChangeContent(() => {
+    codeEditorData.value = model?.getValue();
+  });
 });
 </script>
 
