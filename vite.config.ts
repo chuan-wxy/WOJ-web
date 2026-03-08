f68d7bbdd788ff3d34540a466b1c198ebd4fc74c
chuan_wxy <3153917921@qq.com>
2026-03-08 09:50:42 +0800

    vite.config重构

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,120 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+import ElementPlus from 'unplugin-element-plus/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8088,
-  },
-  resolve: {
-    alias: {
-      // 将 @ 设置为 src 目录
-      '@': path.resolve(__dirname, './src')
+export default ({ mode }: { mode: string }) => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const { VITE_VERSION, VITE_BASE_URL, VITE_PORT, VITE_API_PROXY_URL, VITE_API_URL } = env
+
+  console.log(`🚀 API_URL = ${VITE_API_URL}`)
+  console.log(`🚀 VERSION = ${VITE_VERSION}`)
+
+  return defineConfig({
+    define: {
+      __APP_VERSION__: JSON.stringify(VITE_VERSION)
+    },
+    base: VITE_BASE_URL,
+    server: {
+      port: Number(VITE_PORT),
+      proxy: {
+        '/api': {
+          target: VITE_API_PROXY_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          secure: true
+        },
+        '/csqaq': {
+          target: 'https://api.csqaq.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/csqaq/, '/api'),
+          secure: true
+        }
+      },
+      host: true
+    },
+    plugins: [
+      vue(),
+      tailwindcss(),
+      vueDevTools(),
+      // 自动按需导入 API
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia', '@vueuse/core'],
+        dts: 'src/types/import/auto-imports.d.ts',
+        resolvers: [ElementPlusResolver()],
+        eslintrc: {
+          enabled: true,
+          filepath: './.auto-import.json',
+          globalsPropValue: true
+        }
+      }),
+      // 自动按需导入组件
+      Components({
+        dts: 'src/types/import/components.d.ts',
+        resolvers: [ElementPlusResolver()]
+      }),
+      // 按需定制主题配置
+      ElementPlus({
+        useSource: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@views': resolvePath('src/views'),
+        '@imgs': resolvePath('src/assets/images'),
+        '@icons': resolvePath('src/assets/icons'),
+        '@utils': resolvePath('src/utils'),
+        '@stores': resolvePath('src/store'),
+        '@styles': resolvePath('src/assets/styles'),
+        '@api': resolvePath('src/openapi')
+      }
+    },
+    build: {
+      target: 'es2015',
+      outDir: 'dist',
+      chunkSizeWarningLimit: 2000,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          // 生产环境去除 console
+          drop_console: true,
+          // 生产环境去除 debugger
+          drop_debugger: true
+        }
+      },
+      dynamicImportVarsOptions: {
+        warnOnError: true,
+        exclude: [],
+        include: ['src/views/**/*.vue']
+      }
+    },
+    optimizeDeps: {
+      include: [
+        'echarts/core',
+        'echarts/charts',
+        'echarts/components',
+        'echarts/renderers',
+        'xlsx',
+        'xgplayer',
+        'crypto-js',
+        'file-saver',
+        'vue-img-cutter',
+        'element-plus/es',
+        'element-plus/es/components/*/style/css',
+        'element-plus/es/components/*/style/index'
+      ]
     }
-  }
-})
+  })
+}
+
+function resolvePath(paths: string) {
+  return path.resolve(__dirname, paths)
+}
