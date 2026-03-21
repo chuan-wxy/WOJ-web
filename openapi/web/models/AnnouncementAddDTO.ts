50b433a0432404d2801496ffd10ab7aa7bda4a7c
chuan_wxy <3153917921@qq.com>
2026-03-21 14:49:43 +0800

    feat: 优化题目详情页面

--- openapi/web/models/AnnouncementAddDTO.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type AnnouncementAddDTO = {
-    title?: string;
-    content?: string;
-};
-
